e346a5262ed5e78197303d4af2430bf9f1d648a2
abdul wahab <abdul.wahab394.aw@gmail.com>
2023-05-26 06:56:59 +0500

    app setup

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
