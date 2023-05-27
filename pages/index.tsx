a78a1a102dcc7bf83a6411278ba3d6afd467a021
abdul wahab <abdul.wahab394.aw@gmail.com>
2023-05-27 22:59:26 +0500

    landig page continue..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
-import Layout from "@/components/views/Layout";
+import React from "react";
+import { NextPage } from "next";
 
-// sections
-import HeroSection from "@/components/sections/HeroSection";
 import FlightPriceSection from "@/components/sections/FlightPriceSection";
+import HeroSection from "@/components/sections/HeroSection";
+import Layout from "@/components/views/Layout";
 import SameFlightSection from "@/components/sections/SameFlightSection";
 import EasyToBookSection from "@/components/sections/EasyToBookSection";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <Layout>
-      <div className="bg-light rounded-b-3xl mx-3">
-        <HeroSection />
-        <FlightPriceSection />
-      </div>
-      <SameFlightSection />
-      <EasyToBookSection />
-    </Layout>
+    <>
+      <Layout>
+        <div className="bg-light rounded-b-3xl mx-3">
+          <HeroSection />
+          <FlightPriceSection />
+        </div>
+        <SameFlightSection />
+        <EasyToBookSection />
+      </Layout>
+    </>
   );
-}
+};
+
+export default Home;
