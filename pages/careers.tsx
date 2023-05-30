75053d93d6bf64eccc2b24f4dbeaf6d630e0fbdc
Bilal Tariq <bilaltariq621@gmail.com>
2023-05-30 17:19:52 +0500

    careers page section 1 completed

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -3,53 +3,8 @@ import {NextPage} from 'next';
 import Layout from '@/components/views/Layout';
 import Container from '@/components/views/Container';
 import CareersCard from '@/components/cards/CareersCard';
-import Image from 'next/image';
 
 const Careers: NextPage = () => {
-  const careersCardData = [
-    {
-      style: 'image-only',
-      imageUrl: '/assets/laptop.jpg'
-    },
-    {
-      style: 'icon-text',
-      iconUrl: '/assets/clock.svg',
-      title: 'Flexible hours',
-      description:
-        'Pusit amet luctus venenatis, lectus magna fringilla porttitor us rhoncus dolor purus non enim praesent.'
-    },
-    {
-      style: 'text-only',
-      title: 'Unlimited PTO',
-      description:
-        'Lorem ipsum luctus dolor sit amet, consectetur adipiscing elit ut aliquam, purus non sit amet luctus venenatis, lectus magna fringilla.purus non sit amet luctus venenatis, lectus magna fringilla.enenatis, lectus magna'
-    },
-    {
-      style: 'icon-text',
-      iconUrl: '/assets/remote.svg',
-      title: 'Remote',
-      description:
-        'Pusit amet luctus venenatis, lectus magna fringilla porttitor us rhoncus dolor purus non enim praesent.'
-    },
-    {
-      style: 'image-only',
-      imageUrl: '/assets/plan.jpg'
-    },
-    {
-      style: 'text-only',
-      title: 'Medical insurance',
-      description:
-        'Lorem ipsum luctus dolor sit amet, consectetur adipiscing elit ut aliquam, purus non sit amet luctus venenatis, lectus magna fringilla.purus non sit amet luctus venenatis, lectus magna fringilla.enenatis, lectus magna fringilla.'
-    },
-    {
-      style: 'icon-text',
-      iconUrl: '/assets/heart.svg',
-      title: 'Parental leave',
-      description:
-        'Pusit amet luctus venenatis, lectus magna fringilla porttitor us rhoncus dolor purus non enim praesent.'
-    }
-  ];
-
   return (
     <Layout>
       <div className="max-w-full mx-3 p-6 bg-light rounded-b-3xl">
@@ -74,7 +29,7 @@ const Careers: NextPage = () => {
             <div className="max-w-full xl:mx-auto p-6 pt-0 mb-4">
               <div className="mx-auto max-w-full">
                 <div className="grid grid-cols-1 grid-rows-1 xl:grid-cols-3 xl:grid-rows-3 gap-x-5 lg:gap-x-14 gap-y-4 md:gap-y-2">
-                  <div className="col-span-full md:col-span-1 lg:row-span-2 pt-12 md:pt-12 lg:pt-0  xl:mb-8 ">
+                  <div className="col-span-full md:col-span-1 lg:row-span-2 pt-12 md:pt-12 lg:pt-0  xl:mb-12 ">
                     <img
                       className="xl:w-full xl:h-full object-cover rounded-4xl"
                       src="/assets/laptop.jpg"
